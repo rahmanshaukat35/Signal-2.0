3fa91011792c2146f4b9ad9a8c5527b5a31e752d
rahmanshaukat35 <rahmanshaukat35@gmail.com>
2023-03-09 20:46:05 +0500

    new

--- screens/AddChatScreen.js
+++ screens/AddChatScreen.js
@@ -1,14 +1,53 @@
 import { StyleSheet, Text, View } from "react-native";
-import React from "react";
-
-const AddChatScreen = () => {
+import React, { useLayoutEffect, useState } from "react";
+import { Button, Input } from "react-native-elements";
+import Icon from "react-native-vector-icons/FontAwesome";
+import { db } from "../firebase";
+import { setDoc, doc } from "firebase/firestore";
+const initialState = {
+  input: "",
+};
+const AddChatScreen = ({ navigation }) => {
+  const [state, setState] = useState(initialState);
+  const handleChange = (key, value) => {
+    setState({ ...state, [key]: value });
+  };
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      title: "Add a new Chat",
+      headerBackTitle: "Chats",
+    });
+  }, [navigation]);
+  const randomId = () => {
+    Math.random().toString(36).slice(2);
+  };
+  const createChat = async () => {
+    try {
+      await setDoc(doc(db, "chats", randomId), {
+        chatName: Input,
+      });
+    } catch (err) {
+      console.error(err), alert(err);
+    }
+  };
   return (
-    <View>
-      <Text>AddChatScreen</Text>
+    <View style={styles.container}>
+      <Input
+        placeholder="Enter chat name"
+        value={state.input}
+        onChangeText={(value) => handleChange("input", value)}
+        onSubmitEditing={createChat}
+        leftIcon={
+          <Icon name="wechat" type="antdesign" size={24} color="black" />
+        }
+      />
+      <Button onPress={createChat} title="Create new chat" />
     </View>
   );
 };
 
 export default AddChatScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {},
+});
